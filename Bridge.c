fbd0ebc898669f1409fd068e6d341b87be53518f
Working Tech <k2785233247@gmail.com>
2024-03-03 17:44:38 +0800

    增加了初版的所有注释

--- Bridge.c
+++ Bridge.c
@@ -1,11 +1,45 @@
+/*please use -std=c99 to compile this C*/
+/*This program has failed to achieve the level 3 requirement, there are several bugs when I'm trying to write the descend sorting algorithm. 
+I think my function should be logically correct but it turns out that all of the sorted outputs just mysteriously disappeared.
+
+Example below:
+
+Before:
+(Input)./queue S5 C7 CQ SK HT HA H5 D4 DK D6 S3 C8 S4 S7 HQ DJ H7 SQ C5 D9 S6 HK D8 C4 S8 H6 H8 HJ D2 H4 CT SA H3 C3 D5 ST H2 CK C2 D7 DT S2 H9 D3 DA C9 CJ DQ S9 SJ C6 CA
+(Output) 
+Child 2, pid 1633: C7 HA D6 S7 SQ HK H6 H4 C3 CK S2 C9 SJ
+Child 2, pid 1633: <S7 SQ S2 SJ > <HA HK H6 H4 > <C7 C3 CK C9 > <D6 >
+Child 2, pid 1633: 13 points, 15 adjusted points
+Child 1, pid 1632: S5 HT DK S4 H7 S6 S8 D2 H3 H2 DT DA S9
+Child 1, pid 1632: <S5 S4 S6 S8 S9 > <HT H7 H3 H2 > <> <DK D2 DT DA >
+Child 1, pid 1632: 7 points, 11 adjusted points
+Child 3, pid 1634: CQ H5 S3 HQ C5 D8 H8 CT D5 C2 H9 CJ C6
+Child 3, pid 1634: <S3 > <H5 HQ H8 H9 > <CQ C5 CT C2 CJ C6 > <D8 D5 >
+Child 3, pid 1634: 5 points, 11 adjusted points
+Child 4, pid 1635: SK D4 C8 DJ D9 C4 HJ SA ST D7 D3 DQ CA
+Child 4, pid 1635: <SK SA ST > <HJ > <C8 C4 CA > <D4 DJ D9 D7 D3 DQ >
+Child 4, pid 1635: 15 points, 20 adjusted points
+
+After:
+(Output)
+Child 1, pid 1654: S5 HT DK S4 H7 S6 S8 D2 H3 H2 DT DA S9
+Child 1, pid 1654: <S5 S4 S6 S8 S9 > <HT H7 H3 H2 > <> <DK D2 DT DA >
+Child 2, pid 1655: C7 HA D6 S7 SQ HK H6 H4 C3 CK S2 C9 SJ
+Child 2, pid 1655: <S7 SQ S2 SJ > <HA HK H6 H4 > <C7 C3 CK C9 > <D6 >
+Child 3, pid 1656: CQ H5 S3 HQ C5 D8 H8 CT D5 C2 H9 CJ C6
+Child 3, pid 1656: <S3 > <H5 HQ H8 H9 > <CQ C5 CT C2 CJ C6 > <D8 D5 >
+Child 4, pid 1657: SK D4 C8 DJ D9 C4 HJ SA ST D7 D3 DQ CA
+Child 4, pid 1657: <SK SA ST > <HJ > <C8 C4 CA > <D4 DJ D9 D7 D3 DQ >
+
+I cannot figure out a resolution so I just left all the sorting funtions commented in this source file 
+I've tried, sorry T^T...*/
+/*------------------------------------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h> 
-/*please use -std=c99 to compile this C*/
-/*This program has failed to achieve the descend sorting. There are */
 
 //Custom a Card Object using typedef, containing the suit and the value attributes of the card
 typedef struct
@@ -50,15 +84,15 @@ void ShowPoints(int playerIndex,int points,int adjPoints){
 }
 
 
-// void CardSwap(Card* A,Card* B){
-//     Card* temp;
-//     temp->suit=A->suit;
-//     temp->val=A->val;
-//     A->suit=B->suit;
-//     A->val=B->val;
-//     B->suit=temp->suit;
-//     B->val=temp->val;
-// }
+void CardSwap(Card* A,Card* B){
+    Card* temp;
+    temp->suit=A->suit;
+    temp->val=A->val;
+    A->suit=B->suit;
+    A->val=B->val;
+    B->suit=temp->suit;
+    B->val=temp->val;
+}
 
 //A function that sort the cards in player's hand and calculate the value for the hand
 void SortCard(Card* HandStack,int playerIndex){
@@ -148,7 +182,7 @@ void SortCard(Card* HandStack,int playerIndex){
     }
     adjPoints+=3*Zero+2*Single+Double;
 
-    
+    //Print the Score
     ShowPoints(playerIndex,Valpoints,adjPoints);
 
     
@@ -161,10 +195,7 @@ anything printed on the terminal...*/
 // J=74, Q=81,
 // K=75 (+10) -->85, A=65 (+25) -->90, T=84(-20) -->64 [smallest]
 // So that T<J<Q<K<A
-
-    // InnerSort(Sstack,playerIndex,Scount);
     
-
     // for(int i=0;i<Scount-1;i++){
     //     for(int j=i+1;j<Scount;j++){
     //         char pre=Sstack[i].val;
@@ -187,9 +218,7 @@ anything printed on the terminal...*/
     //         }
     //     }
     // }
-    // LimitShow(Sstack,playerIndex,Scount);
         
-
     // for(int i=0;i<Hcount-1;i++){
     //     for(int j=i+1;j<Hcount;j++){
     //         char pre=Hstack[i].val;
@@ -261,52 +290,42 @@ anything printed on the terminal...*/
     //     }
     // }
         
-
-    // printf("<");
-    // for(int i=0;i<Scount;i++){
-    // printf("%c%c ",Sstack[i].suit,Sstack[i].val);
-    // }
-    // printf("> \n");
-
-
     printf("\n");
-    // printf("S:%d H:%d C:%d D:%d \n",Scount,Hcount,Ccount,Dcount);
 
 
 }
 
 int main(int argc, char *argv[]){
 
-    // //已知会有52张牌被存入argv[]中(所有花色牌)
+    //Known that a stack of 52 cards will be input in the command line
     Card Stack[52];
+    //Store all the input cards.
     for(int i=0;i<argc-1;i++){
         Stack[i].suit=argv[i+1][0];
         Stack[i].val=argv[i+1][1];
     }
 
-    // Card HandStack[13];
-    // Distribute(Stack,HandStack,0);
-    // ShowCard(HandStack,0);
+    //Using for-loop to create 4 players(childs)
     for(int i=0;i<4;i++){
     int player=fork();
     if(player<0){
         printf("Failed to create the player, the program will terminate now.");
         return 0;
-    }else if(player==0){
-        Card HandStack[13]; //建立当前玩家手牌[container]
-        Distribute(Stack,HandStack,i); //给当前玩家发牌
-        ShowCard(HandStack,i); //展示当前玩家所持有的卡
-        SortCard(HandStack,i);
-        return 0;
+    }else if(player==0){//What would be done to each player
+        Card HandStack[13]; //Construct the hand stack for the player
+        Distribute(Stack,HandStack,i); //Extract the specific card from the Stack to player's hand
+        ShowCard(HandStack,i); //Initially print the player's hand
+        SortCard(HandStack,i); //Group and Calculate the player's hand
+        return 0; //termination of a child process
     }
             
     }
 
-    //receive exit status from the 4 players
+    //receive exit status from the 4 players (parent)
     for(int i=0;i<4;i++){
         wait(NULL);
     }
 
-
+    printf("I've written some problems that I encountered in the source code, please check it.\n");
 }
 
